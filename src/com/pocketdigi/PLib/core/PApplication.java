17c240d77bce178cf202c19a050e9735aafeb17d
pocketdigi <webmaster@pocketdigi.com>
2014-08-29 18:34:56 +0800

    2.0

---
+++ src/com/pocketdigi/PLib/core/PApplication.java
@@ -0,0 +1,16 @@
+package com.pocketdigi.PLib.core;
+
+import android.app.Application;
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivityGroup;
+import de.greenrobot.event.EventBus;
+
+/**
+ * Application基类
+ * Created by fhp on 14-8-28.
+ */
+public class PApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
